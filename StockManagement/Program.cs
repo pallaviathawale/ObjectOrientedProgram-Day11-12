b46321aff566462ea2e2892a191be0f665a575d7
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-04-01 00:31:04 +0530

    Add: StockManagement

---
+++ StockManagement/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StockManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StockPortfolio portfolio = new StockPortfolio();
+
+            // Read input from user
+            Console.Write("Enter the number of stocks: ");
+            int numStocks = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numStocks; i++)
+            {
+                Console.Write("Enter stock name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter number of shares: ");
+                int numShares = int.Parse(Console.ReadLine());
+                Console.Write("Enter share price: ");
+                double price = double.Parse(Console.ReadLine());
+                Stock stock = new Stock { Name = name, NumShares = numShares, Price = price };
+                portfolio.AddStock(stock);
+            }
+
+            // Print stock report
+            Console.WriteLine(portfolio.GetStockReport());
+        }
+    }
+}
+        
+    
---
+++ StockManagement/StockPortFolio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement
+{
+    internal class StockPortfolio
+    {
+        private List<Stock> stocks = new List<Stock>();
+
+        public void AddStock(Stock stock)
+        {
+            stocks.Add(stock);
+        }
+        public string GetStockReport()
+        {
+            double totalValue = 0;
+            string report = "";
+            foreach (Stock stock in stocks)
+            {
+                double value = stock.GetValue();
+                totalValue += value;
+                report += $"{stock.Name}: {stock.NumShares} shares worth {value}\n";
+            }
+            report += $"Total value of all stocks: {totalValue}";
+            return report;
+        }
+    }
+}
+
