08f59f8f5265c2aa249242ddaa7d28fc71b02927
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-04-01 01:10:12 +0530

    Add: UC2-InventoryDataMangement

---
+++ JsonInventoryDataManagement/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonInventoryDataManagement
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            InventoryManager manager = new InventoryManager();
+            string file = @"C:\Users\PALLAVI\source\repos\StockManagement\JsonInventoryDataManagement\Datamanager.json";
+            InventoryUtility utility = JsonConvert.DeserializeObject<InventoryUtility>(File.ReadAllText(file));
+            Console.WriteLine("Display inventory Press 1-Rice Inventory 2-Wheat Inventory 3-Pulse Inventory ");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    manager.DisplayInventory(utility.RiceList);
+                    break;
+                case 2:
+                    manager.DisplayInventory(utility.WheatList);
+                    break;
+                case 3:
+                    manager.DisplayInventory(utility.PulseList);
+                    break;
+            }
+        }
+
+    }
+}
